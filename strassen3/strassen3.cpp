d4ee1546f572982d5654e1d5cc14f6e68d3bec65
jkulma <01161330@pw.edu.pl>
2025-04-19 19:17:11 +0200

    init commit

---
+++ strassen3/strassen3.cpp
@@ -0,0 +1,12 @@
+﻿// strassen3.cpp : Defines the entry point for the application.
+//
+
+#include "strassen3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
